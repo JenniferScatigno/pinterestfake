b52cb2b49e08fe142558ea0305ae04bd383fe55e
JenniferScatigno <107508502+JenniferScatigno@users.noreply.github.com>
2023-08-23 13:55:17 -0300

    Add files via upload

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidades.db"
+app.config["SECRET_KEY"] = "76c1938e968382bd249af2218825f05b"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
+
+from fakepinterest import routes
+
